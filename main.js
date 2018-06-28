f2b714de253099180a6934b0ffdb86f673e620d3
Marco Afonso <mafonso333@gmail.com>
2018-06-28 12:31:42 +0100

    Fix emoji mouse behaviour

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function goImage(){
-	document.getElementById('.emo'+number).src=('personaje'+number+'.jpg');
+function goImage(number){
+	document.getElementById('emo'+number).src=('personaje'+number+'.jpg');
 }
-function comeImage(){
-	document.getElementById('.emo1'+number).src=('emoji'+number+'.png');
+function comeImage(number){
+	document.getElementById('emo'+number).src=('emoji'+number+'.png');
 }
