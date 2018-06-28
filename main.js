6f22d0ebd3842515e58b01a21a6dc5f5747ae1a5
SofiaRincon <38178498+SofiaRinconArtMath@users.noreply.github.com>
2018-06-28 10:15:47 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+function goImage(){
+	document.getElementById('.emo1').src='personaje1';
+}
+function comeImage(){
+	document.getElementById('.emo1').src='emoji1.jpg';
+}
+function goImage(){
+	document.getElementById('.emo2').src='personaje2.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo2').src='emoji2.jpg';
+}
+function goImage(){
+	document.getElementById('.emo3').src='personaje3.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo3').src='emoji3.jpg';
+}
+function goImage(){
+	document.getElementById('.emo4').src='personaje4.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo4').src='emoji4.jpg';
+}
+function goImage(){
+	document.getElementById('.emo5').src='personaje5.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo5').src='emoji5.jpg';
+}
+function goImage(){
+	document.getElementById('.emo6').src='personaje6.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo6').src='emoji6.jpg';
+}
+function goImage(){
+	document.getElementById('.emo7').src='personaje7.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo7').src='emoji7.jpg';
+}
+function goImage(){
+	document.getElementById('.emo8').src='personaje8.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo8').src='emoji8.jpg';
+}
+function goImage(){
+	document.getElementById('.emo9').src='personaje9.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo9').src='emoji9.jpg';
+}
+function goImage(){
+	document.getElementById('.emo10').src='personaje10.jpg';
+
+function comeImage(){
+	document.getElementById('.emo10').src='emoji10.jpg';
+}
+function goImage(){
+	document.getElementById('.emo11').src='personaje11.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo11').src='emoji11.jpg';
+}
+function goImage(){
+	document.getElementById('.emo12').src='personaje12.jpg';
+}
+function comeImage(){
+	document.getElementById('.emo12').src='emoji12.jpg';
+}
